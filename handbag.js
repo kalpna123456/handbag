7ee6d9fd179a3b91100d9f9473367a75a0f5f224
kalpna devi <kpsuthar996@gmail.com>
2025-12-09 21:04:17 +0500

    Add files via upload

---
+++ handbag.js
@@ -0,0 +1,71 @@
+ function changeimg() {
+        document.getElementById("myimg").src="handbag img copy1.png";
+    }
+    function changeimgOne(){
+             document.getElementById("myimgOne").src="handbag img 6.jpg";
+    }
+    function changeimgTwo(){
+         document.getElementById("myimgTwo").src="handbag img copy 3.jpg";
+    }
+    function changeimgThree(){
+        document.getElementById("myimgThree").src="handbag img copy 4.webp"
+    }
+     function changeimgFor(){
+        document.getElementById("myimgFor").src="handbag img copy 5.jpg"
+    }
+      function changeimgFive(){
+        document.getElementById("myimgFive").src="handbag img 2.jpg"
+    }
+     function changeimgSix(){
+        document.getElementById("myimgSix").src="handbag img copy 7.png"
+    }
+     function changeimgSiven(){
+        document.getElementById("myimgSiven").src="handbag img copy 8.jpg"
+    }
+     function changeimgEight(){
+        document.getElementById("myimgEight").src="handbag img copy 9.jpg"
+    }
+    function changeimgNinth(){
+        document.getElementById("myimgNinth").src="handbag img copy 10.jpg"
+    }
+    
+    function changeimgThanth(){
+        document.getElementById("myimgThanth").src="handbag img copy 11.png"
+    }
+    
+    function changeimgElevent(){
+        document.getElementById("myimgElevent").src="handbag img copy 12.jpg"
+    }
+     function changePic(){
+        document.getElementById("myPic").src="handbag copy 1.jpg"
+    }
+    function changePicOne(){
+        document.getElementById("myPicOne").src="handbag copy 2.jpg"
+    }
+     function changePicTwo(){
+        document.getElementById("myPicTwo").src="handbag copy 3.png"
+    }
+     function changePicThree(){
+        document.getElementById("myPicThree").src="handbag copy 4.jpg"
+    }
+     function changePicFor(){
+        document.getElementById("myPicFor").src="handbag copy 5.jpg"
+    }
+     function changePicFive(){
+        document.getElementById("myPicFive").src="handbag copy 6.jpg"
+    }
+     function changePicSix(){
+        document.getElementById("myPicSix").src="handbag copy 7.jpg"
+    }
+     function changePicSiven(){
+        document.getElementById("myPicSiven").src="handbag copy 8.jpg"
+    }
+    function changePicEight(){
+        document.getElementById("myPicEight").src="handbag copy 9.jpg"
+    }
+    function changePicNinth(){
+        document.getElementById("myPicNinth").src="handbag copy 10.jpg"
+    }
+    
+
+    
